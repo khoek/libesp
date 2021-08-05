5894f9484060ef091a477fe11d09878dbe9ee49b
Keeley Hoek <keeley@hoek.io>
2021-08-06 06:15:53 +1000

    Update

--- include/libesp.h
+++ include/libesp.h
@@ -1,10 +1,10 @@
-#ifndef __LIB_LIBESP_H
-#define __LIB_LIBESP_H
+#pragma once
 
 #include <esp_err.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
+#include <sys/cdefs.h>
 
 // DANGER: `micros` should be small to avoid WDT triggers.
 void util_wait_micros(int64_t micros);
@@ -17,8 +17,16 @@ esp_err_t util_stack_overflow_check();
 
 void util_stack_print_remaining();
 
-static __always_inline bool util_unterm_str_matches(const char* st, const char* sut, size_t sut_len) {
+// WARNING: You must set `CONFIG_FREERTOS_RUN_TIME_STATS_USING_ESP_TIMER=y` in
+// order to use this function.
+void util_freertos_print_usage();
+
+static __always_inline bool util_unterm_str_matches(const char* st,
+                                                    const char* sut,
+                                                    size_t sut_len) {
     return !strncmp(st, sut, sut_len) && st[sut_len] == '\0';
 }
 
-#endif
+static __always_inline void ESP_ERROR_DISCARD(esp_err_t err) {
+    // Do nothing.
+}
--- src/libesp/util.c
+++ src/libesp/util.c
@@ -24,16 +24,15 @@ void util_wait_micros(int64_t micros) {
 
 void util_wait_for_keypress() {
     // Clear the buffer
-    while (fgetc(stdin) != EOF)
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+    while (fgetc(stdin) != EOF) vTaskDelay(10 / portTICK_PERIOD_MS);
 
     // Now wait until next key
-    while (fgetc(stdin) == EOF)
-        vTaskDelay(10 / portTICK_PERIOD_MS);
+    while (fgetc(stdin) == EOF) vTaskDelay(10 / portTICK_PERIOD_MS);
 }
 
 void util_log_idf_version() {
-    ESP_LOGW(TAG, "%d.%d.%d", ESP_IDF_VERSION_MAJOR, ESP_IDF_VERSION_MINOR, ESP_IDF_VERSION_PATCH);
+    ESP_LOGW(TAG, "%d.%d.%d", ESP_IDF_VERSION_MAJOR, ESP_IDF_VERSION_MINOR,
+             ESP_IDF_VERSION_PATCH);
 }
 
 esp_err_t util_stack_overflow_check() {
@@ -49,5 +48,46 @@ esp_err_t util_stack_overflow_check() {
 }
 
 void util_stack_print_remaining() {
-    ESP_LOGW(TAG, "stack words remaining: %u", uxTaskGetStackHighWaterMark(NULL));
+    ESP_LOGW(TAG, "stack words remaining: %u",
+             uxTaskGetStackHighWaterMark(NULL));
+}
+
+void util_freertos_print_usage() {
+#if configUSE_TRACE_FACILITY
+    UBaseType_t num_tasks = uxTaskGetNumberOfTasks();
+    TaskStatus_t *pxTaskStatusArray =
+        pvPortMalloc(num_tasks * sizeof(TaskStatus_t));
+    if (pxTaskStatusArray == NULL) {
+        return;
+    }
+
+    ESP_LOGW(TAG, "Format: %16s | %8s | %s", "name", "run time", "usage(%)");
+
+    uint32_t total_run_time;
+    num_tasks =
+        uxTaskGetSystemState(pxTaskStatusArray, num_tasks, &total_run_time);
+    if (total_run_time > 0) {
+        for (UBaseType_t i = 0; i < num_tasks; i++) {
+            uint64_t usage_percent =
+                ((uint64_t) pxTaskStatusArray[i].ulRunTimeCounter) * 100ULL
+                / ((uint64_t) total_run_time);
+            if (usage_percent > 0UL) {
+                ESP_LOGW(TAG, "%16s   %8u   %llu%%",
+                         pxTaskStatusArray[i].pcTaskName,
+                         pxTaskStatusArray[i].ulRunTimeCounter, usage_percent);
+            } else {
+                ESP_LOGW(TAG, "%16s   %8u   <1%%",
+                         pxTaskStatusArray[i].pcTaskName,
+                         pxTaskStatusArray[i].ulRunTimeCounter);
+            }
+        }
+    }
+
+    vPortFree(pxTaskStatusArray);
+#else
+    ESP_LOGE(TAG,
+             "You must set `CONFIG_FREERTOS_RUN_TIME_STATS_USING_ESP_TIMER=y` "
+             "in order to use this function.");
+    abort();
+#endif
 }
